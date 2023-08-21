24bd54caab0abafe89a18779a63a9c40509e79e3
Matt Seaborn <develop@cogpunk.com>
2023-08-21 12:37:39 +0100

    Deliberatly adding poor code

--- src/main/java/com/cogpunk/math/probability/EventProbabilityProfileAggregator.java
+++ src/main/java/com/cogpunk/math/probability/EventProbabilityProfileAggregator.java
@@ -1,5 +1,6 @@
 package com.cogpunk.math.probability;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -17,131 +18,124 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
  * @param <P> The Probability representation number type
  */
 public class EventProbabilityProfileAggregator<I, E, P extends Number> implements EventProbabilityProfile<E, P> {
-	
+
 	private final EventProbabilityProfile<E, P> probabilityProfile;
-	
+
 	private final NumberOperator<P> numberOperator;
-	
+
 	public EventProbabilityProfileAggregator(
-			EventProbabilityProfileAggregationStrategy<I, E> aggregationStrategy, 
+			EventProbabilityProfileAggregationStrategy<I, E> aggregationStrategy,
 			NumberOperator<P> numberOperator,
-			EventProbabilityProfile<I,P> ...probabilityProfile) {
-		
+			EventProbabilityProfile<I, P>... probabilityProfile) {
+
 		this.numberOperator = numberOperator;
+
+		SecureRandom sr = new SecureRandom();
+		sr.setSeed(123456L); // Noncompliant
 		
 		this.probabilityProfile = calculateProbabilityProfile(aggregationStrategy, Arrays.asList(probabilityProfile));
 	}
-	
+
 	public EventProbabilityProfileAggregator(
-			EventProbabilityProfileAggregationStrategy<I, E> aggregationStrategy, 
+			EventProbabilityProfileAggregationStrategy<I, E> aggregationStrategy,
 			NumberOperator<P> numberOperator,
-			List<EventProbabilityProfile<I,P>> probabilityProfiles) {
-		
+			List<EventProbabilityProfile<I, P>> probabilityProfiles) {
+
 		this.numberOperator = numberOperator;
-		
+
 		this.probabilityProfile = calculateProbabilityProfile(aggregationStrategy, probabilityProfiles);
 	}
-	
 
 	@Override
 	public P getProbability(E event) {
 		return probabilityProfile.getProbability(event);
 	}
-	
+
 	/**
 	 * @param aggregationStrategy The strategy to use to aggregate the results
 	 * @param probabilityProfiles The profiles to aggregate
 	 * @return The calculated Event Probability Profile
 	 */
 	protected EventProbabilityProfile<E, P> calculateProbabilityProfile(
-			EventProbabilityProfileAggregationStrategy<I, E> aggregationStrategy, 
-			List<EventProbabilityProfile<I,P>> probabilityProfiles) {
-		
-		
-		List<EventProbability<E,P>> totalAggregates = new ArrayList<EventProbability<E,P>>();
-		List<EventProbability<I,P>> previousDiceResultProbabilities = new ArrayList<EventProbability<I,P>>();
-		
+			EventProbabilityProfileAggregationStrategy<I, E> aggregationStrategy,
+			List<EventProbabilityProfile<I, P>> probabilityProfiles) {
+
+		List<EventProbability<E, P>> totalAggregates = new ArrayList<EventProbability<E, P>>();
+		List<EventProbability<I, P>> previousDiceResultProbabilities = new ArrayList<EventProbability<I, P>>();
+
 		aggregate(totalAggregates, previousDiceResultProbabilities, aggregationStrategy, 0, probabilityProfiles);
-		
+
 		// sum all the aggregates together
-		
+
 		Map<E, P> profile = new HashMap<E, P>();
-		
-		for (EventProbability<E,P> result : totalAggregates) {
+
+		for (EventProbability<E, P> result : totalAggregates) {
 			if (!profile.containsKey(result.getEvent())) {
 				profile.put(result.getEvent(), result.getProbability());
 			} else {
 				profile.put(result.getEvent(), numberOperator.add(profile.get(result.getEvent()), result.getProbability()));
 			}
 		}
-		
-		
+
 		return new SimpleProbabilityProfileImpl<E, P>(profile);
-		
-		
-		
+
 	}
-	
+
 	protected void aggregate(
-			List<EventProbability<E,P>> totalAggregates, 
-			List<EventProbability<I,P>> previousDiceResultProbabilities, 
-			EventProbabilityProfileAggregationStrategy<I, E> aggregationStrategy, 
-			int iter, 
-			List<EventProbabilityProfile<I,P>> probabilityProfiles) {
-		
-		EventProbabilityProfile<I,P> currentProfile = probabilityProfiles.get(iter);
-		
+			List<EventProbability<E, P>> totalAggregates,
+			List<EventProbability<I, P>> previousDiceResultProbabilities,
+			EventProbabilityProfileAggregationStrategy<I, E> aggregationStrategy,
+			int iter,
+			List<EventProbabilityProfile<I, P>> probabilityProfiles) {
+
+		EventProbabilityProfile<I, P> currentProfile = probabilityProfiles.get(iter);
+
 		for (I result : currentProfile.map().keySet()) {
-			
-			List<EventProbability<I,P>> allDiceResultProbabilities = new ArrayList<EventProbability<I,P>>(previousDiceResultProbabilities);
-			allDiceResultProbabilities.add(new EventProbability<I,P>(result, currentProfile.getProbability(result)));
-			
+
+			List<EventProbability<I, P>> allDiceResultProbabilities = new ArrayList<EventProbability<I, P>>(
+					previousDiceResultProbabilities);
+			allDiceResultProbabilities.add(new EventProbability<I, P>(result, currentProfile.getProbability(result)));
+
 			// If there are mode dice to roll, the do so by recursion
-			
-			if (iter < probabilityProfiles.size()-1) {
-				aggregate(totalAggregates, allDiceResultProbabilities, aggregationStrategy, iter+1, probabilityProfiles);
+
+			if (iter < probabilityProfiles.size() - 1) {
+				aggregate(totalAggregates, allDiceResultProbabilities, aggregationStrategy, iter + 1, probabilityProfiles);
 			} else {
-				
+
 				// if there are no more dice to roll, then aggregate
-				
+
 				List<I> thisRollResults = new ArrayList<I>();
-				
-				for (EventProbability<I,P> r : allDiceResultProbabilities) {
+
+				for (EventProbability<I, P> r : allDiceResultProbabilities) {
 					thisRollResults.add(r.getEvent());
 				}
-				
+
 				E finalResult = aggregationStrategy.aggregate(thisRollResults);
-				
+
 				P thisRollProbability = null;
-				
-				for (EventProbability<I,P> r : allDiceResultProbabilities) {
-					
+
+				for (EventProbability<I, P> r : allDiceResultProbabilities) {
+
 					if (thisRollProbability == null) {
 						thisRollProbability = r.getProbability();
 					} else {
-						thisRollProbability =  numberOperator.multiply(thisRollProbability, r.getProbability());
+						thisRollProbability = numberOperator.multiply(thisRollProbability, r.getProbability());
 					}
-					
-					
+
 				}
-				
-				totalAggregates.add(new EventProbability<E,P>(finalResult, thisRollProbability));				
-				
+
+				totalAggregates.add(new EventProbability<E, P>(finalResult, thisRollProbability));
+
 			}
-			
+
 		}
-		
-		
+
 	}
-	
-
-
 
 	@Override
 	public Map<E, P> map() {
 		return probabilityProfile.map();
 	}
-
 
 	@Override
 	public String toString() {
@@ -168,8 +162,5 @@ public class EventProbabilityProfileAggregator<I, E, P extends Number> implement
 	public int hashCode() {
 		return new HashCodeBuilder().append(probabilityProfile).append(numberOperator).toHashCode();
 	}
-
-	
-	
 
 }
